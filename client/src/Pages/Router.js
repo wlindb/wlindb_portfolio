d80a11fca43bba1499966b2cc52657555d10dd0a
wlindb <wille.lindblom@gmail.com>
2020-12-20 19:17:42 +0100

    Added lazy load

--- client/src/Pages/Router.js
+++ client/src/Pages/Router.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './Navbar';
-import Landing from './Landing';
-import About from './About';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
+import Loading from '../components/Loading';
+
+const Landing = lazy(() => import('./Landing'));
+const Portfolio = lazy(() => import('./Portfolio'));
+const Contact = lazy(() => import('./Contact'));
 
 const Router = () => {
     return (
-        <>
-            <BrowserRouter>
-                <Navbar/>
+        <BrowserRouter>
+            <Navbar/>
+            <Suspense fallback={<Loading/>}>
                 <Switch>
                     <Route exact path="/home" component={Landing} ></Route>
-                    <Route exact path="/about" name='About' component={About} ></Route>
                     <Route exact path="/portfolio/" name='Portfolio' component={Portfolio} ></Route>
                     <Route exact path="/contact" name='Contact' component={Contact} ></Route>
                     <Route component={Landing}/>
                 </Switch>
-            </BrowserRouter>
-        </>
+            </Suspense>
+        </BrowserRouter>
     )
 }
 
